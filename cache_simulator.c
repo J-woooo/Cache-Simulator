997a6da34538a55a39880514619328b2f0503293
Bae Jin Woo <jw969696@naver.com>
2020-07-25 16:39:18 +0900

    add multi-way

--- cache_simulator.c
+++ cache_simulator.c
@@ -9,7 +9,6 @@
 typedef enum { false, true } bool;
 
 
-
 ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
 
 int next_use(int input[], int input_cnt, int value, int curr_idx) 
@@ -55,17 +54,17 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 		for (i = 0; i < input_cnt; i++) { 
 			int loc = input[i] % set_count; 
 			if (cache[loc][0] == 0) { // No value exists there, so add it.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				cache[loc][0] = input[i]; 
 				cache[loc][1] = i;
 				cache_misses++;
 			}   
 			else if (cache[loc][0] == input[i]) { // HIT, update refresh value in cache
-				printf("%d (hit)\n", input[i]);
+				//printf("%d (hit)\n", input[i]);
 				cache[loc][1] = i;
 				cache_hits++;
 			} else { // Replace cache block with new value.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				cache[loc][0] = input[i];
 				cache[loc][1] = i;
 				cache_misses++;
@@ -85,34 +84,34 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 		for (i = 0; i < input_cnt; i++) {
 			int loc = input[i] % set_count;
 			if (cache[loc][0] == 0 && cache[loc][1] == 0) { // No value is there now, so add one.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				cache[loc][0] = input[i];
 				cache[loc][2] = i;
 				cache_misses++;
 			}
 			else if (cache[loc][0] == input[i]) { // Hit with 1st number.
-				printf("%d (hit)\n", input[i]);
+				//printf("%d (hit)\n", input[i]);
 				cache[loc][2] = i;
 				cache_hits++;
 			}
 			else if (cache[loc][1] == input[i]) { // Hit with 2nd number.
-				printf("%d (hit)\n", input[i]);
+				//printf("%d (hit)\n", input[i]);
 				cache[loc][3] = i;
 				cache_hits++;
 			}
 			else if (cache[loc][0] == 0) { // 1st value is only one available, hit.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				cache[loc][0] = input[i];
 				cache[loc][2] = i;
 				cache_misses++;
 			}
 			else if (cache[loc][1] == 0 && cache[loc][0] != 0) { // 2nd value is only one available, hit.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				cache[loc][1] = input[i];
 				cache[loc][3] = i;
 				cache_misses++;
 			} else { // Replace LRU.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				if (cache[loc][2] <= cache[loc][3]) { // Replace the 1st number and new stage.
 					cache[loc][0] = input[i];
 					cache[loc][2] = i;
@@ -124,7 +123,7 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 			}
 		}
 	} 
-	else {
+	else if (associativity == 4){
 		cache = (int **)malloc( sizeof(int *) * set_count );
 		for (i = 0; i < set_count; i++) { // First 4 idx's are values, second 4 are LRU stages.
 			cache[i] = (int *)malloc( sizeof(int) * 8 );
@@ -139,7 +138,7 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 			int loc = input[i] % set_count;
 			for (j = 0; j < 4; j++) {
 				if (input[i] == cache[loc][j]) { // Hit, update LRU.
-					printf("%d (hit)\n", input[i]);
+					//printf("%d (hit)\n", input[i]);
 					cache[loc][j+4] = i;
 					cache_hits++;
 					found = true;
@@ -149,13 +148,13 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 				}
 			}
 			if (cnt == 0 && !found) { // Whole set is empty, so add it.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				cache[loc][0] = input[i];
 				cache[loc][4] = i;
 				cache_misses++;
 			} 
 			else if (cnt > 0 && cnt < 4 && !found) { // There is at least one empty block.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				int j;
 				for (j = 0; j < 4; j++) {
 					if (cache[loc][j] == 0) { // Found an empty slot.
@@ -167,7 +166,7 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 				cache_misses++;
 			} 
 			if (cnt == 4 && !found) { // All slots are full so replace LRU.
-				printf("%d (miss)\n", input[i]);
+				//printf("%d (miss)\n", input[i]);
 				int min = 99999, idx = 0, j = 0;
 				for (j = 0; j < 4; j++) {
 					if (cache[loc][j+4] <= min && cache[loc][j] != 0) {  
@@ -181,139 +180,120 @@ void LRU_cache(int input[], int input_cnt, int associativity)
 			}	
 		}
 	}
-
-	// Output cache statistics.
-	float hit_rate = (float)cache_hits / (float)cache_accesses;
-	printf("Cache accesses: %d\n", cache_accesses);
-	printf("Cache hits: %d\n", cache_hits);
-	printf("Cache misses: %d\n", cache_misses);
-	printf("Overall hit rate: %f\n", hit_rate);
-
-	// Free dynamically allocated memory.
-	for (i = 0; i < set_count; i++) {
-		free(cache[i]);
-	}
-	free(cache);
-}
-
-
-///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
-
-void Belady_cache(int input[], int input_cnt, int associativity)
-	/*\
-	 * This function simulates the Belady cache.
- 	\*/
-{
-	// Initialize and declare output variables.
-	int cache_accesses = input_cnt;
-	int cache_hits = 0;
-	int cache_misses = 0;
-	int i = 0;
-
-	int **cache;
-	int set_count = 256 / associativity; 
-	cache = (int **)malloc( sizeof(int *) * set_count );
-	if (associativity == 1) { // Allocate direct-mapped cache. Set to 0.
-		for (i = 0; i < set_count; i++) {
-			cache[i] = (int *)malloc( sizeof(int) * 2 );
-			cache[i][0] = 0;
-			cache[i][1] = 0;
+	else if(associativity == 8){
+		cache = (int **)malloc(sizeof(int *) * set_count);
+		for (i = 0; i < set_count; i++) { // First 4 idx's are values, second 4 are LRU stages.
+			cache[i] = (int *)malloc(sizeof(int) * 16);
+			int k;
+			for (k = 0; k < 16; k++) { // Initialize to all 0.
+				cache[i][k] = 0;
+			}
 		}
-	}
-	else if (associativity == 2) { // Allocate 2-Way associative cache. Set to 0.
-		for (i = 0; i < set_count; i++) {
-			cache[i] = (int *)malloc( sizeof(int) * 4 );
-			cache[i][0] = 0;
-			cache[i][1] = 0;
-			cache[i][2] = 0;
-			cache[i][3] = 0;
-		}
-	} else { // Allocate 4-Way associative cache. Set to 0.
-		for (i = 0; i < set_count; i++) {
-			cache[i] = (int *)malloc( sizeof(int) * 8 );
-			int j;
-			for (j = 0; j < 8; j++) { // Initialize to 0.
-				cache[i][j] = 0;
+		for (i = 0; i < input_cnt; i++) {
+			bool found = false;
+			int cnt = 0, j = 0;
+			int loc = input[i] % set_count;
+			for (j = 0; j < 8; j++) {
+				if (input[i] == cache[loc][j]) { // Hit, update LRU.
+					//printf("%d (hit)\n", input[i]);
+					cache[loc][j + 8] = i;
+					cache_hits++;
+					found = true;
+				}
+				if (cache[loc][j] != 0) {
+					cnt++;
+				}
+			}
+			if (cnt == 0 && !found) { // Whole set is empty, so add it.
+				//printf("%d (miss)\n", input[i]);
+				cache[loc][0] = input[i];
+					cache[loc][8] = i;
+					cache_misses++;
+				}
+				else if (cnt > 0 && cnt < 8 && !found) { // There is at least one empty block.
+					//printf("%d (miss)\n", input[i]);
+					int j;
+					for (j = 0; j < 8; j++) {
+						if (cache[loc][j] == 0) { // Found an empty slot.
+							cache[loc][j] = input[i];
+							cache[loc][j + 8] = i;
+							break;
+						}
+					}
+					cache_misses++;
+				}
+				if (cnt == 8 && !found) { // All slots are full so replace LRU.
+					//printf("%d (miss)\n", input[i]);
+					int min = 99999, idx = 0, j = 0;
+				for (j = 0; j < 8; j++) {
+					if (cache[loc][j + 8] <= min && cache[loc][j] != 0) {
+						min = cache[loc][j + 8];
+						idx = j;
+					}
+				}
+				cache[loc][idx] = input[i];
+				cache[loc][idx + 8] = i;
+				cache_misses++;
 			}
 		}
 	}
-	for (i = 0; i < input_cnt; i++) { // Loop through the input and search cache.
-		int loc = input[i] % set_count;
-		int j = 0, cnt = 0;
+	else if (associativity == 16) {
+	cache = (int **)malloc(sizeof(int *) * set_count);
+	for (i = 0; i < set_count; i++) { // First 4 idx's are values, second 4 are LRU stages.
+		cache[i] = (int *)malloc(sizeof(int) * 32);
+		int k;
+		for (k = 0; k < 16; k++) { // Initialize to all 0.
+			cache[i][k] = 0;
+		}
+	}
+	for (i = 0; i < input_cnt; i++) {
 		bool found = false;
-		for (j = 0; j < associativity; j++) {
-			if (input[i] == cache[loc][j] && !found) { // Hit, refresh for next time it's accessed.
-				printf("%d (hit)\n", input[i]);
-				cache[loc][j+associativity] = next_use(input, input_cnt, input[i], i);
+		int cnt = 0, j = 0;
+		int loc = input[i] % set_count;
+		for (j = 0; j < 16; j++) {
+			if (input[i] == cache[loc][j]) { // Hit, update LRU.
+				//printf("%d (hit)\n", input[i]);
+				cache[loc][j + 16] = i;
 				cache_hits++;
-			    found = true;
+				found = true;
 			}
 			if (cache[loc][j] != 0) {
 				cnt++;
 			}
 		}
-		if (cnt == 0 && !found) { // First value to be entered.
-			printf("%d (miss)\n", input[i]);
-			cache[loc][0] = input[i]; 
-			cache[loc][associativity] = next_use(input, input_cnt, input[i], i);
+		if (cnt == 0 && !found) { // Whole set is empty, so add it.
+			//printf("%d (miss)\n", input[i]);
+			cache[loc][0] = input[i];
+			cache[loc][16] = i;
 			cache_misses++;
 		}
-		else if (cnt > 0 && cnt < associativity && !found) { // There is an available space, so add it.
-			printf("%d (miss)\n", input[i]);
-			bool added = false;
-			cache_misses++;
-			for (j = 0; j < associativity; j++) {
-				if (cache[loc][j] == 0 && !added) {
+		else if (cnt > 0 && cnt < 16 && !found) { // There is at least one empty block.
+			//printf("%d (miss)\n", input[i]);
+			int j;
+			for (j = 0; j < 16; j++) {
+				if (cache[loc][j] == 0) { // Found an empty slot.
 					cache[loc][j] = input[i];
-					cache[loc][j + associativity] = next_use(input, input_cnt, input[i], i);
-					added = true; 
+					cache[loc][j + 16] = i;
+					break;
 				}
 			}
-		} 
-		// Cases:
-		// 1 - 4 values appear again, choose most distance.
-		// 2 - 1 value does not appear again.
-		// 3 - Multiple values do not appear again.
-		else if (cnt == associativity && !found) { // Replace a value.
-			int use = -1, idx = 0, not_found_cnt = 0;
-			printf("%d (miss)\n", input[i]);
 			cache_misses++;
-			for (j = 0; j < associativity; j++) {
-				if (cache[loc][j+associativity] > use) {
-					use = cache[loc][j + associativity];
+		}
+		if (cnt == 16 && !found) { // All slots are full so replace LRU.
+			//printf("%d (miss)\n", input[i]);
+			int min = 99999, idx = 0, j = 0;
+			for (j = 0; j < 16; j++) {
+				if (cache[loc][j + 8] <= min && cache[loc][j] != 0) {
+					min = cache[loc][j + 16];
 					idx = j;
 				}
-				if (cache[loc][j+associativity] == -1) { // If not found in the future.
-					not_found_cnt++;
-				}
-			} // Add in the value in the right location.
-			if (not_found_cnt > 1) {
-				int min = 99999;
-				idx = 0;
-				for (j = 0; j < associativity; j++) {
-					if (cache[loc][j + associativity] == -1 && cache[loc][j] < min) {
-						min = cache[loc][j];
-						idx = j;
-					}
-				}
-				cache[loc][idx] = input[i];
-				cache[loc][idx + associativity] = next_use(input, input_cnt, input[i], i);
 			}
-			else if (not_found_cnt == 1) {
-				for (j = 0; j < associativity; j++) {
-					if (cache[loc][j+associativity] == -1) {
-						cache[loc][j] = input[i];
-						cache[loc][j + associativity] = next_use(input, input_cnt, input[i], i);
-						break;
-					}
-				}
-			} else {
-				cache[loc][idx] = input[i];
-				cache[loc][idx + associativity] = next_use(input, input_cnt, input[i], i); 
-			}
+			cache[loc][idx] = input[i];
+			cache[loc][idx + 16] = i;
+			cache_misses++;
 		}
 	}
-
+	}
 	// Output cache statistics.
 	float hit_rate = (float)cache_hits / (float)cache_accesses;
 	printf("Cache accesses: %d\n", cache_accesses);
@@ -327,6 +307,7 @@ void Belady_cache(int input[], int input_cnt, int associativity)
 	}
 	free(cache);
 }
+
 
 
 ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
@@ -351,10 +332,10 @@ int main(int argc, char *argv[])
 	int associativity = atoi(argv[1]);
 	int cache_sets;
 	char *algorithm = argv[2];
-	bool is_LRU = false, is_BELADY = false;
+	bool is_LRU = false;
 
 	// If invalid associativity value, exit program.
-	if ( associativity != 1 && associativity != 2 && associativity != 4 ) { 
+	if ( associativity != 1 && associativity != 2 && associativity != 4 && associativity != 8 && associativity != 16) {
 		fprintf(stderr, "Error: Invalid Associativity Entered!\n");
 		return EXIT_FAILURE;
 	}
@@ -362,9 +343,7 @@ int main(int argc, char *argv[])
 	if ( strcmp(algorithm, "LRU") == 0 ) { // LRU Algorithm 
 		is_LRU = true;
 	}
-	else if ( strcmp(algorithm, "Belady") == 0 ) { // Belady Algorithm
-		is_BELADY = true;
-	} else { // Invalid Entry
+	else { // Invalid Entry
 		fprintf(stderr, "Error: Invalid Algorithm Entered!\n");
 		return EXIT_FAILURE;
 	}
@@ -386,9 +365,7 @@ int main(int argc, char *argv[])
 
 	if (is_LRU)
 		LRU_cache(input, input_cnt, associativity);
-	if (is_BELADY)
-		Belady_cache(input, input_cnt, associativity);
-
+	
 	free(input);
 	return EXIT_SUCCESS;
 }
